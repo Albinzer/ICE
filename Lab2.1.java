32b0ab4f0ed211565382cc4941fd6d8f8149f45a
Abu-Bakkar Siddik Zilhaj <160333632+Albinzer@users.noreply.github.com>
2024-05-22 16:42:19 +0600

    Update Lab2.1.java
    Lab experiment in java

--- Lab2.1.java
+++ Lab2.1.java
@@ -1,2 +1,38 @@
-class Triangle {
-  
+import java.util.Scanner;  //package
+
+//This is class
+public class Triangle{
+    double width,height;
+
+  //this is constructor
+   public Triangle(double width,double height){
+        this.width=width;
+        this.height=height;
+    }
+
+    //this is method
+    public double calculateArea(){
+        return 0.5*width*height;
+    }
+
+  //this is main function
+    public static void main(String[] args){
+       double width,height;
+       
+       Scanner scanner=new Scanner(System.in);
+       
+       System.out.println("Enter the value of width =");
+       width=scanner.nextDouble();
+       
+       System.out.println("enter the value of height =");
+       height=scanner.nextDouble();
+
+      //this is object
+       Triangle triangle=new  Triangle (width,height);
+
+       System.out.println("Area of the triangle" + triangle.calculateArea());
+       
+       scanner.close();
+       
+    }
+}
