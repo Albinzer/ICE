f089c170d84304a6f2ee61f3f49d4a3bcd9464f4
Abu-Bakkar Siddik Zilhaj <160333632+Albinzer@users.noreply.github.com>
2024-05-23 01:22:01 +0600

    Create lab 2.java
    lab 2

---
+++ lab 2.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+// CLASS
+public class NumSum{
+    int num1,num2,num3;
+
+  //
+    NumSum(int n1,int n2,int n3){
+        this.num1=n1;
+        this.num2=n2;
+        this.num3=n3;
+    }
+     public int calculateSum (){
+        return num1+num2+num3;
+    }
+
+    public static void main(String[] args){
+        int num1,num2,num3;
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter the value of num1");
+        num1=scanner.nextInt();
+        System.out.println("Enter the value of num2");
+        num2=scanner.nextInt();
+        System.out.println("Enter the value of num3");
+        num3=scanner.nextInt();
+    NumSum nsum=new NumSum(num1,num2,num3);
+    System.out.println("Sum of the three number" + nsum.calculateSum());
+    scanner.close ();
+}
+}
