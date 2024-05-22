1fd9262d862f6e89c740067f73cd5c27feaf9d23
Abu-Bakkar Siddik Zilhaj <160333632+Albinzer@users.noreply.github.com>
2024-05-22 15:14:04 +0600

    Update lab2.1.cpp
    Lab experiment-22/5/2024

--- lab2.1.cpp
+++ lab2.1.cpp
@@ -1,1 +1,37 @@
 #include <iostream>
+using namespace std;
+
+// this is class
+class Triangle{
+public:
+
+float width,height;
+
+//this is constructor
+Triangle(float w,float h){
+width=w;
+height=h;
+}
+
+//this is method
+float calculateArea() {
+return 0.5*width*height;
+}
+};
+
+//this is main function
+int main() {
+float width,height;
+  
+cout << ("Enter the value of a width=");
+cin >> width;
+cout << ("Enter the value of a height=");
+cin >> height ;
+
+//this is object
+Triangle tri (width,height);
+cout << "Area of the Triangle" << tri.calculateArea() << endl;
+return 0;
+}
+
+
