85d95316fed27b9b287a2310c5e2a533e9010370
Abu-Bakkar Siddik Zilhaj <160333632+Albinzer@users.noreply.github.com>
2024-05-23 01:58:22 +0600

    Create lab3.java
    lab3

---
+++ lab3.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class MaxMin {
+    int num1, num2;
+
+    MaxMin(int n1, int n2) {
+        this.num1 = n1;
+        this.num2 = n2;
+    }
+
+    void findMaxMin() {
+        if (num1 > num2) {
+            System.out.println("Maximum: " + num1);
+            System.out.println("Minimum: " + num2);
+        } else {
+            System.out.println("Maximum: " + num2);
+            System.out.println("Minimum: " + num1);
+        }
+    }
+
+    public static void main(String[] args) {
+        int num1, num2;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the value of num1:");
+        num1 = scanner.nextInt();
+        System.out.println("Enter the value of num2:");
+        num2 = scanner.nextInt();
+        MaxMin maxmin = new MaxMin(num1, num2);
+        maxmin.findMaxMin();
+        scanner.close();
+    }
+}
