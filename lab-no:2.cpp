80e1130768c34850a36c1255913cf33153f39f10
Abu-Bakkar Siddik Zilhaj <160333632+Albinzer@users.noreply.github.com>
2024-05-23 00:54:16 +0600

    Update and rename lab no. 2 to lab-no:2.cpp
    lab 2

--- lab-no:2.cpp
+++ lab-no:2.cpp
@@ -1,23 +1,25 @@
 #include<iostream>
 using namespace std;
 
-
+//this is a class
  class Numsum{
      public:
-
-
     int num1,num2,num3;
+
+//this is a constructor
     Numsum( int n1,int n2,int n3){
          num1=n1;
          num2=n2;
          num3=n3;
     }
 
+//this is actually a method
     int  calculateSum(){
          return num1+num2+num3;
     }
  };
 
+//this is a main function
     int main(){
         int num1,num2,num3;
         
@@ -27,7 +29,8 @@ using namespace std;
         cin>> num2;
         cout <<"Enter the value of num3:";
         cin>> num3;
-        
+
+     // this is a object
         Numsum sum(num1,num2,num3);
         cout<<"Sum of the three value"<<sum.calculateSum() << endl;
         return 0;
