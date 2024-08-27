619a0f03befe64bb47239981b1a3561dd03ca4c2
Abu-Bakkar Siddik Zilhaj <160333632+Albinzer@users.noreply.github.com>
2024-08-28 00:31:56 +0600

    Create lab 9.cpp

---
+++ lab 9.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows = 4, coef = 1;
+
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j <= i; j++) {
+            if (j == 0 || i == 0)
+                coef = 1;
+            else
+                coef = coef * (i - j + 1) / j;
+
+            cout << coef << " ";
+        }
+    }
+    cout << endl;
+    return 0;
+}
