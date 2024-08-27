059d2555438f02abe2884f8ad32262c4575a2cca
Abu-Bakkar Siddik Zilhaj <160333632+Albinzer@users.noreply.github.com>
2024-08-28 01:07:18 +0600

    Create lab 10.java

---
+++ lab 10.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+class GPACalculator {
+
+    public float calculateGPA(int[] marks, int numSubjects) {
+        float total = 0;
+        for (int mark : marks) {
+            total += mark;
+        }
+        return total / numSubjects / 10.0f; // Assuming the GPA scale is out of 10
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of subjects: ");
+        int numSubjects = sc.nextInt();
+
+        int[] marks = new int[numSubjects];
+        for (int i = 0; i < numSubjects; i++) {
+            System.out.print("Enter marks for subject " + (i + 1) + ": ");
+            marks[i] = sc.nextInt();
+        }
+
+        GPACalculator gpaCalculator = new GPACalculator();
+        float gpa = gpaCalculator.calculateGPA(marks, numSubjects);
+
+        System.out.println("Your GPA is: " + gpa);
+
+        sc.close();
+    }
+}
