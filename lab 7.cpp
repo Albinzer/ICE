c25d8a0c77c5ff0b4ce6788c46f4e8fbbbae7049
Abu-Bakkar Siddik Zilhaj <160333632+Albinzer@users.noreply.github.com>
2024-08-28 00:29:13 +0600

    Create lab 7.cpp

---
+++ lab 7.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+class Student {
+public:
+    string name;
+    int rollNumber;
+    float marks;
+
+    void inputDetails() {
+        cout << "Enter student name: ";
+        getline(cin, name);
+        cout << "Enter roll number: ";
+        cin >> rollNumber;
+        cout << "Enter marks: ";
+        cin >> marks;
+    }
+
+    void displayDetails() {
+        cout << "\nStudent Details:\n";
+        cout << "Name: " << name << endl;
+        cout << "Roll Number: " << rollNumber << endl;
+        cout << "Marks: " << marks << endl;
+    }
+};
+
+int main() {
+    Student student;
+    cin.ignore();
+    student.inputDetails();
+    student.displayDetails();
+    return 0;
+}
